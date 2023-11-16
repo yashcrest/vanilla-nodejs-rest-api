487959d8989d8f01df864a16f7fa25a5b82c2162
yashcrest <yashcrest@gmail.com>
2023-11-16 19:05:48 +1030

    r

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,38 +1,35 @@
-const Product = require('../models/produtModel');
+const Product = require("../models/produtModel");
 
 // @desc Gets all products
 // @route GET /api/product/:id
 async function getProducts(req, res) {
-    try {
-        const products = await Product.findAll()
-        res.writeHead(200, {'Content-Type' : 'application/json'})
-        res.end(JSON.stringify(products));
-    }
-    catch(error){
-        console.log(error);
-    }
+  try {
+    const products = await Product.findAll();
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(products));
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // @desc Gets single product
 // @route GET /api/product/:id
 async function getProduct(req, res, id) {
-    try {
-        const product = await Product.findById(id)
-        if(!product){
-            res.writeHead(404, {'Content-Type' : 'application/json'})
-            res.end(JSON.stringify({message: 'Product not found'}));
-        } else {
-            res.writeHead(200, {'Content-Type' : 'application/json'})
-            res.end(JSON.stringify(product));
-        }
-        
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Product not found" }));
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(product));
     }
-    catch(error){
-        console.log(error);
-    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = {
-    getProducts,
-    getProduct
-}
+  getProducts,
+  getProduct,
+};
