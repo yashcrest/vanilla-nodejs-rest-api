940793001fd2f45251931d65f20063f02762fd82
yashcrest <yashcrest@gmail.com>
2023-11-18 23:56:26 +1030

    changing the local server port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const {
   createProduct,
   updateProduct,
 } = require("./controllers/productController");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 //creating an instance of http
 const server = http.createServer((req, res) => {
